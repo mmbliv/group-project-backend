a0d4c2c66107f7b56e111330867d9f2f113ff4a7
locb65 <locb65@gmail.com>
2023-03-21 18:25:59 -0400

    created controllers for updating by id and creating recipes

--- App/Controllers/Controller.js
+++ App/Controllers/Controller.js
@@ -4,4 +4,12 @@ const findRecipe = async () => {
     return await recipes.find()
 }
 
+const updateRecipe = async (id) => {
+    return await recipes.findOneAndUpdate(id)
+}
+
+const createRecipe = async () => {
+    return await recipes.create()
+}
+
 
