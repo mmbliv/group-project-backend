bc55902aa1c029abaf422429bd8f9d3ef1507fb1
min <mmbliv@gmail.com>
2023-03-21 15:29:07 -0500

    add timestamps to Groceries

--- App/Models/GlossaryModel.js
+++ App/Models/GlossaryModel.js
@@ -1,12 +1,16 @@
+import { timeStamp } from "console";
 import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
-const Groceries = new Schema({
-  name: String,
-  created_at: Date,
-  deleted: Boolean,
-  checked: Boolean,
-  recipe: { type: Schema.Types.ObjectId, ref: "Recipe" },
-});
+const Groceries = new Schema(
+  {
+    name: String,
+    //   created_at: Date,
+    deleted: Boolean,
+    checked: Boolean,
+    recipe: { type: Schema.Types.ObjectId, ref: "Recipe" },
+  },
+  { timeStamp: true }
+);
 
 export default mongoose.model("Groceries", Groceries);
