a872c0880166236842c60fd8aede10127e99ff11
locb65 <locb65@gmail.com>
2023-03-21 22:18:16 -0400

    created basic routes and updater server to host groceries route

---
+++ App/Routes/GroceriesRouter.js
@@ -0,0 +1,12 @@
+import groceriesController from "../Controllers/GroceriesControllers";
+import express from 'express'
+
+const groceriesRouter = express.Router();
+
+groceriesRouter.get('/', groceriesController.getAllGroceries);
+groceriesRouter.get('/:id', groceriesController.getGroceries);
+groceriesRouter.post('/', groceriesController.createGroceries);
+groceriesRouter.put('/:id', groceriesController.updateGroceries);
+groceriesRouter.delete('/:id', groceriesController.deleteGroceries);
+
+export default groceriesRouter;
--- App/server.js
+++ App/server.js
@@ -1,12 +1,15 @@
 import express from 'express';
 import errorHandler from './Helpers/errorHandler.js';
 import recipeRouter from './Routes/RecipeRouter.js';
+import groceriesRouter from './Routes/GroceriesRouter.js';
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/recipes", recipeRouter);
+
+app.use("/groceries", groceriesRouter);
 
 app.use(errorHandler);
 
