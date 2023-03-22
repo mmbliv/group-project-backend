0fabe5559062130e17ae86a2c0b920805a7e9163
Loc Bui <buda@Locs-MacBook-Pro.local>
2023-03-22 08:51:28 -0400

    updated groceriesControllers to catch errors

--- App/Controllers/GroceriesControllers.js
+++ App/Controllers/GroceriesControllers.js
@@ -1,28 +1,33 @@
 import Groceries from '../Models/Groceries.js';
 
 const groceriesController = {
-    getAllGroceries: async (req, res) => {
+    getAllGroceries: async (req, res, next) => {
         Groceries.find({})
         .then(groceries => res.json(groceries))
+        .catch(err => next(err))
     },
-    getGroceries: async (req, res) => {
+    getGroceries: async (req, res, next) => {
         const id = req.params.id;
         Groceries.findById(id)
       .then(groceries => res.json(groceries))
+      .catch(err => next(err))
     },
-    createGroceries: async (req, res) => {
+    createGroceries: async (req, res, next) => {
         const newGroceries = new Groceries(req.body);
-        res.json(newGroceries);
+        res.json(newGroceries)
+        .catch(err => next(err))
     },
-    deleteGroceries: async (req, res) => {
+    deleteGroceries: async (req, res, next) => {
         const id = req.params.id
         .then(groceries => res.json(groceries))
+        .catch(err => next(err))
     },
-    updateGroceries: async (req, res) => {
+    updateGroceries: async (req, res, next) => {
         const id = req.params.id
         Groceries.findByIdAndUpdate(id, req.body)
-        .then(groceries => {res.json(groceries
-        )})
+        .then(groceries => {res.json(groceries)
+        .catch(err => next(err))
+        })
     }
 };
 
