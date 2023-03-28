fbd999d5f089040b689b717264356889a575678a
min <mmbliv@gmail.com>
2023-03-28 10:06:57 -0500

    added updateCheck method

--- App/Controllers/GroceriesControllers.js
+++ App/Controllers/GroceriesControllers.js
@@ -23,6 +23,14 @@ const groceriesController = {
       res.json(groceries);
     });
   },
+  updateCheck: async (req, res) => {
+    const id = req.params.id;
+    const data = await Groceries.findById(id);
+    const updatedData = await Groceries.findByIdAndUpdate(id, {
+      checked: !data.checked,
+    });
+    res.json(updatedData);
+  },
 };
 
 export default groceriesController;
--- App/Routes/GroceriesRouter.js
+++ App/Routes/GroceriesRouter.js
@@ -8,5 +8,6 @@ groceriesRouter.get("/:id", groceriesController.getGroceries);
 groceriesRouter.post("/", groceriesController.createGroceries);
 groceriesRouter.put("/:id", groceriesController.updateGroceries);
 groceriesRouter.delete("/:id", groceriesController.deleteGroceries);
+groceriesRouter.put("/check/:id", groceriesController.updateCheck);
 
 export default groceriesRouter;
