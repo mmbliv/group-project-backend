2228d90a2be99d983e6941baa1224c4c5654d3af
min <mmbliv@gmail.com>
2023-03-31 14:49:14 -0500

    deploy

--- src/db/connections.js
+++ src/db/connections.js
@@ -1,5 +1,8 @@
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb://localhost/Recipes", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(
+  "mongodb+srv://mmbliv:1040hemin@cluster0.3id8zfg.mongodb.net/?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
 
-export default mongoose
+export default mongoose;
