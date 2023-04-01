51b9e9eff88c9e4ee02e2c6548fe7798db7db1f2
min <mmbliv@gmail.com>
2023-04-01 13:45:31 -0500

    deploy

--- src/server.js
+++ src/server.js
@@ -3,9 +3,10 @@ import cors from "cors";
 import errorHandler from "./Helpers/errorHandler.js";
 import recipeRouter from "./Routes/RecipeRouter.js";
 import groceriesRouter from "./Routes/GroceriesRouter.js";
-
+import * as dotenv from "dotenv";
+import connectDB from "./db/connections.js";
+dotenv.config();
 const app = express();
-
 const corsOption = {
   origin: "*",
   credentials: true,
@@ -21,7 +22,15 @@ app.use("/recipes", recipeRouter);
 app.use("/groceries", groceriesRouter);
 
 app.use(errorHandler);
-
-app.listen(8080, () => {
-  console.log("listening");
-});
+const PORT = 4000 || process.env.PORT;
+const Start = async () => {
+  try {
+    await connectDB(process.env.MONGODB);
+    app.listen(PORT, () => {
+      console.log("listening");
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+Start();
